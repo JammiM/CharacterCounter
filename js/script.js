dd3be91d329a4ab5b967cff3ca22ffb14585d23c
JammiM <jammim19@gmail.com>
2016-10-29 14:35:00 +0100

    Adding the ability to read the textarea field

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
+  var testResult, testResultLength;
+
+  $('#calBtn').click(function(){
+
+    testResult = $('#textfield').val();
+    $( "#result" ).append( "<strong>"  + testResult.length +"</strong>" );
+
+  }); //.click
+
+
 });
